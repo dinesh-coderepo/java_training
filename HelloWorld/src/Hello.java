36bf881161f832d365d34edbf7a2664c9ed8f915
coderapp <maluchurudinesh@gmail.com>
2019-04-08 11:52:33 +0530

    Adding helo world java program

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+
+    public static void  main(String[] args){
+
+        System.out.println("You suck!");
+
+        int val1 = (5+1)*12;
+        System.out.println(val1);
+
+
+
+
+    }
+}

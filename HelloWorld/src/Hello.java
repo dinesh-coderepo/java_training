f0255c1326d4be98a5ebd1b4401d32fc95b5e5ec
Dinesh <maluchurudinesh@gmail.com>
2022-02-16 17:56:00 +0530

    cleaning

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,14 +1,10 @@
 public class Hello {
 
     public static void  main(String[] args){
-
-        System.out.println("You suck!");
-
+        System.out.println("Hello world!");
         int val1 = (5+1)*12;
         System.out.println(val1);
 
-
-
-
     }
 }
+
